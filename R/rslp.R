768759c09f313b6faabc94e2103c0f6188cf3572
Daniel Falbel <dfalbel@gmail.com>
2016-06-30 18:20:41 -0400

    warnings are not erros in build. just for this work fow now

--- R/rslp.R
+++ R/rslp.R
@@ -7,8 +7,7 @@
 #' @param steprules as obtained from the function extract_rules. (only define if you are certain about it).
 #' The default is to get the parsed versionof the rules installed with the package.
 #'
-#' @references
-#' \url{http://homes.dcc.ufba.br/~dclaro/download/mate04/Artigo%20Erick.pdf}
+#' @references \url{http://homes.dcc.ufba.br/~dclaro/download/mate04/Artigo%20Erick.pdf}
 #'
 #' @examples
 #' words <- c("balões", "aviões", "avião", "gostou", "gosto", "gostaram")
